5a2552300582eb116df626da28c532cfe2907b7e
Arisha-Developer <arisharehman1224@gmail.com>
2022-10-20 23:00:04 +0500

    first commit

---
+++ public/app.js
@@ -0,0 +1,222 @@
+ var userdata=prompt("Enter your roll no:")
+ 
+ var student1={
+    name:"Sarah",
+    rollNo:210,
+    contactNo:123456,
+    percentage:89,
+}
+ if(userdata==210){
+    document.getElementById("name").innerHTML =
+    student1.name;
+    document.getElementById("roll_no").innerHTML =
+    student1.rollNo;
+    document.getElementById("cont_no").innerHTML =
+    student1.contactNo;
+    document.getElementById("perc").innerHTML =
+    student1.percentage;
+ }
+
+ var student2={
+    name:"Ali",
+    rollNo:211,
+    contactNo:123456,
+    percentage:85,
+}
+ if(userdata==211){
+    document.getElementById("name").innerHTML =
+    student2.name;
+    document.getElementById("roll_no").innerHTML =
+    student1.rollNo;
+    document.getElementById("cont_no").innerHTML =
+    student1.contactNo;
+    document.getElementById("perc").innerHTML =
+    student1.percentage;
+ }
+ 
+ var student3={
+    name:"Anum",
+    rollNo:212,
+    contactNo:123456,
+    percentage:89,
+}
+ if(userdata==212){
+    document.getElementById("name").innerHTML =
+    student3.name
+    document.getElementById("roll_no").innerHTML =
+    student1.rollNo;
+    document.getElementById("cont_no").innerHTML =
+    student1.contactNo;
+    document.getElementById("perc").innerHTML =
+    student1.percentage;
+ }
+ var student4={
+    name:"Sairah",
+    rollNo:213,
+    contactNo:123456,
+    percentage:89,
+}
+ if(userdata==213){
+    document.getElementById("name").innerHTML =
+    student4.name
+    document.getElementById("roll_no").innerHTML =
+    student4.rollNo;
+    document.getElementById("cont_no").innerHTML =
+    student4.contactNo;
+    document.getElementById("perc").innerHTML =
+    student4.percentage;
+ }
+ 
+ var student5={
+    name:"Sadia",
+    rollNo:214,
+    contactNo:123456,
+    percentage:80,
+}
+ if(userdata==214){
+    document.getElementById("name").innerHTML =
+    student5.name
+    document.getElementById("roll_no").innerHTML =
+    student5.rollNo;
+    document.getElementById("cont_no").innerHTML =
+    student5.contactNo;
+    document.getElementById("perc").innerHTML =
+    student5.percentage;
+ }
+ 
+ var student6={
+    name:"Sairah",
+    rollNo:215,
+    contactNo:123456,
+    percentage:75,
+}
+ if(userdata==215){
+    document.getElementById("name").innerHTML =
+    student6.name
+    document.getElementById("roll_no").innerHTML =
+    student6.rollNo;
+    document.getElementById("cont_no").innerHTML =
+    student6.contactNo;
+    document.getElementById("perc").innerHTML =
+    student6.percentage;
+ }
+
+ var student7={
+    name:"Nadia",
+    rollNo:216,
+    contactNo:123456,
+    percentage:84,
+}
+ if(userdata==216){
+    document.getElementById("name").innerHTML =
+    student7.name
+    document.getElementById("roll_no").innerHTML =
+    student7.rollNo;
+    document.getElementById("cont_no").innerHTML =
+    student7.contactNo;
+    document.getElementById("perc").innerHTML =
+    student7.percentage;
+ }
+
+ var student8={
+    name:"Iqra",
+    rollNo:217,
+    contactNo:123456,
+    percentage:78,
+}
+ if(userdata==217){
+    document.getElementById("name").innerHTML =
+    student8.name
+    document.getElementById("roll_no").innerHTML =
+    student8.rollNo;
+    document.getElementById("cont_no").innerHTML =
+    student8.contactNo;
+    document.getElementById("perc").innerHTML =
+    student8.percentage;
+ }
+ 
+ var student9={
+    name:"Hira",
+    rollNo:218,
+    contactNo:123456,
+    percentage:89,
+}
+ if(userdata==218){
+    document.getElementById("name").innerHTML =
+    student9.name
+    document.getElementById("roll_no").innerHTML =
+    student9.rollNo;
+    document.getElementById("cont_no").innerHTML =
+    student9.contactNo;
+    document.getElementById("perc").innerHTML =
+    student9.percentage;
+ }
+
+ var student10={
+    name:"Nida",
+    rollNo:219,
+    contactNo:123456,
+    percentage:89,
+}
+ if(userdata==219){
+    document.getElementById("name").innerHTML =
+    student10.name
+    document.getElementById("roll_no").innerHTML =
+    student10.rollNo;
+    document.getElementById("cont_no").innerHTML =
+    student10.contactNo;
+    document.getElementById("perc").innerHTML =
+    student10.percentage;
+ }
+ 
+ var student11={
+    name:"Nadir",
+    rollNo:220,
+    contactNo:123456,
+    percentage:90,
+}
+ if(userdata==220){
+    document.getElementById("name").innerHTML =
+    student11.name
+    document.getElementById("roll_no").innerHTML =
+    student11.rollNo;
+    document.getElementById("cont_no").innerHTML =
+    student11.contactNo;
+    document.getElementById("perc").innerHTML =
+    student11.percentage;
+ }
+
+ var student12={
+    name:"Kiran",
+    rollNo:221,
+    contactNo:123456,
+    percentage:89,
+}
+ if(userdata==221){
+    document.getElementById("name").innerHTML =
+    student12.name
+    document.getElementById("roll_no").innerHTML =
+    student12.rollNo;
+    document.getElementById("cont_no").innerHTML =
+    student12.contactNo;
+    document.getElementById("perc").innerHTML =
+    student12.percentage;
+ }
+
+ var student13={
+    name:"Bilal",
+    rollNo:222,
+    contactNo:123456,
+    percentage:89,
+}
+ if(userdata==222){
+    document.getElementById("name").innerHTML =
+    student13.name
+    document.getElementById("roll_no").innerHTML =
+    student13.rollNo;
+    document.getElementById("cont_no").innerHTML =
+    student13.contactNo;
+    document.getElementById("perc").innerHTML =
+    student13.percentage;
+ }
+ 
